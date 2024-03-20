8d470a814e02bd8c9212b8fc11a1e54daaac541a
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-03-20 16:25:24 -0500

    Creating an 'add to cart' feature

---
+++ JSproj/cart.js
@@ -0,0 +1,42 @@
+let cartQuantity = 0;
+
+showQuantity = () => {
+    console.log(`Cart quantitiy: ${cartQuantity}`);
+};
+
+addToCart = () => {
+    console.log(`Cart quantitiy: ${cartQuantity += 1}`);
+};
+
+add2ToCart = () => {
+    console.log(`Cart quantitiy: ${cartQuantity += 2}`);
+};
+
+add3ToCart = () => {
+    console.log(`Cart quantitiy: ${cartQuantity += 3}`);
+};
+
+removeFromCart = () => {
+    if (cartQuantity == 0) {
+        console.log(`Cart quantity is ${cartQuantity}! Nothing to remove from cart.`);
+    }else {
+        console.log(`Cart quantity: ${cartQuantity -= 1 }`);
+    }
+};
+
+resetCart = () => {
+    if (cartQuantity == 0) {
+        console.log(`Cart quantity is ${cartQuantity}! Cart reset successful`);
+    } else {
+    console.log('Cart was reset!');
+    console.log(`Cart quantitiy: ${cartQuantity = 0}`);
+    }
+};
+
+const cost = 2 * 3 + 5 + 9;
+const tax = cost * .1;
+const totalCost = cost + tax;
+
+console.log(`Cost of food: $${cost}`);
+console.log(`Cost of tax: $${tax}`);
+console.log(`total cost: $${totalCost}`);
