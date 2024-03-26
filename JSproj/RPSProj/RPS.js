a5b06cdffd95b231d9e07eec92ab6b74b8df8f52
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-03-26 08:57:29 -0500

    added 'resetScore' feature

--- JSproj/RPSProj/RPS.js
+++ JSproj/RPSProj/RPS.js
@@ -22,19 +22,24 @@ playerMove = (pChoice) => {
     let compChoice = computerMove();
     if (compChoice === pChoice) {
         countScore.Ties += 1;
-        result = alert(`Computer chose ${compChoice} - It's a tie!\n${score()}`);
+        result = console.log(`Computer chose ${compChoice} - It's a tie!\n${score()}`);
     } else if (compChoice === "Scissors" && pChoice === "Rock" || compChoice === "Rock" && pChoice === "Paper" || compChoice === "Paper" && pChoice === "Scissors") {
         countScore.Wins += 1;
-        result = alert(`Computer chose ${compChoice} - ${pChoice} beats ${compChoice}, you win!\n${score()}`);
+        result = console.log(`Computer chose ${compChoice} - ${pChoice} beats ${compChoice}, you win!\n${score()}`);
     } else if (compChoice === "Paper" && pChoice === "Rock" || compChoice === "Rock" && pChoice === "Scissors" || compChoice === "Scissors" && pChoice === "Paper") {
         countScore.Losses += 1;
-        result = alert(`Computer chose ${compChoice} - ${compChoice} beats ${pChoice}, you lose!\n${score()}`);
+        result = console.log(`Computer chose ${compChoice} - ${compChoice} beats ${pChoice}, you lose!\n${score()}`);
     };
     return result;
 };
 
 score = () => {
     return (`Wins: ${countScore.Wins}, Losses: ${countScore.Losses}, Ties: ${countScore.Ties}`)
+};
+
+resetScore = () => {
+    countScore.Wins = 0; countScore.Losses = 0; countScore.Ties = 0;
+    console.log(score());
 };
 
 pickRock = () => {
