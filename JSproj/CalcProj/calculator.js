63eecf5aceab447c52dba5c275216ea42c987fa4
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-05-03 20:35:38 -0500

    refactored code for better input tracking

--- JSproj/CalcProj/calculator.js
+++ JSproj/CalcProj/calculator.js
@@ -1,95 +1,27 @@
-// let calculation = "";
-
-// putOne = () => {
-//     console.log(`${calculation += 1}`);
-// };
-
-// putTwo = () => {
-//     console.log(`${calculation += 2}`);
-// };
-
-// putThree = () => {
-//     console.log(`${calculation += 3}`);
-// };
-
-// putFour = () => {
-//     console.log(`${calculation += 4}`);
-// };
-
-// putFive = () => {
-//     console.log(`${calculation += 5}`);
-// };
-
-// putSix = () => {
-//     console.log(`${calculation += 6}`);
-// };
-
-// putSeven = () => {
-//     console.log(`${calculation += 7}`);
-// };
-
-// putEight = () => {
-//     console.log(`${calculation += 8}`);
-// };
-
-// putNine = () => {
-//     console.log(`${calculation += 9}`);
-// };
-
-// putZero = () => {
-//     console.log(`${calculation += 0}`);
-// };
-
-// addSymbol = () => {
-//     console.log(`${calculation += " + "}`);
-// };
-
-// addDecimal = () => {
-//     console.log(`${calculation += "."}`);
-// };
-
-// subtractSymbol = () => {
-//     console.log(`${calculation += " - "}`);
-// };
-
-// multiplySymbol = () => {
-//     console.log(`${calculation += " * "}`);
-// };
-
-// divideSymbol = () => {
-//     console.log(`${calculation += " / "}`);
-// };
-
-// calculate = () => {
-//     calculation = eval(calculation);
-//     console.log(calculation);
-//     calculation = "";
-// };
-
 let calculation = localStorage.getItem("calculation") || "";
-console.log(calculation);
 
-putNumber = (number) => {
-    console.log(`${calculation += number}`);
-    localStorage.setItem("calculation", calculation);
+printResults = () => {
+    const display = document.querySelector(".showResults");
+    display.innerHTML = calculation;
 };
 
-putSymbol = (symbol) => {
-    if (!calculation) {
-        calculation = 0
-    }
-    console.log(`${calculation += symbol}`);
+printResults();
+
+updateCalculation = (input) => {
+    calculation += input;
+
+    printResults();
     localStorage.setItem("calculation", calculation);
 };
 
 calculate = () => {
     calculation = eval(calculation);
-    console.log(calculation);
+    printResults();
     localStorage.setItem("calculation", calculation);
 };
 
 reset = () => {
-    calculation = "";
-    console.log(calculation);
+    calculation = 0;
+    printResults();
     localStorage.setItem("calculation", calculation);
 };
