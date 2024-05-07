13600397f8dfdb5ff5a8e89a6cca401227368f37
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-05-07 11:45:54 -0500

    refactored: added dateInput and an object to the array

--- JSproj/TodoProj/Todo.js
+++ JSproj/TodoProj/Todo.js
@@ -1,11 +1,16 @@
-const todoList = [];
+const todoList = [{}];
 
 displayOnPage = () => {
     let todoListHTML = "";
 
     for(i=0;i<todoList.length; i++) {
         const todoItem = todoList[i];
-        const html = `<p>${todoItem}</p>`;
+        const { name, dueDate } = todoItem;
+        const html = `
+        <p>
+        ${name} ${dueDate}
+        <button onclick="removeFromList();">Delete</button>
+        </p>`;
         todoListHTML += html;
         document.querySelector(".addToDiv").innerHTML = todoListHTML;
     };
@@ -13,10 +18,18 @@ displayOnPage = () => {
 
 addToList = () => {
     const textInput = document.querySelector(".inputText");
-    const itemName = textInput.value;
-    todoList.push(itemName);
+    const name = textInput.value;
+    const dateInput = document.querySelector(".inputDate");
+    const dueDate = dateInput.value;
+
+    todoList.push({name, dueDate});
     console.log(todoList);
     textInput.value = "";
 
+    displayOnPage();
+};
+
+removeFromList = (i) => {
+    todoList.splice(i, 1);
     displayOnPage();
 };
