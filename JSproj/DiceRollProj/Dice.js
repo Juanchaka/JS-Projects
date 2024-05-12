66ac7ac24fe2d358cb1b757c0a82c97702afe651
Juanchaka <162772765+Juanchaka@users.noreply.github.com>
2024-05-11 21:16:19 -0500

    edited Tempproj name && added DiceRollproj

---
+++ JSproj/DiceRollProj/Dice.js
@@ -0,0 +1,15 @@
+rollDice = () => {
+    const numberODice = document.querySelector("#inputText").value;
+    const result = document.querySelector("#results");
+    const diceImages = document.querySelector("#diceImages");
+    const values = [];
+    const images = [];
+
+    for (i = 0; i < numberODice; i++) {
+        const value = Math.floor(Math.random() * 6) + 1;
+        values.push(value);
+        images.push(`<img src="/DiceImages/${value}.png" alt="Dice: ${value}">`);
+    };
+    result.innerHTML = `Dice: ${values.join(", ")}`;
+    diceImages.innerHTML = images.join("");
+};
